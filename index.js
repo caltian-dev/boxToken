9419865d1edd66db35745d4ecb1d7f8c719f7f63
Caleb Tian <calebtian.dev@gmail.com>
2025-01-23 12:24:59 -0500

    modify refresh token third

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const querystring = require("querystring");
 const CLIENT_ID = "o7xpgssqmi6ztyurh7icr8orqq6asrbt"; // Replace with your client ID
 const CLIENT_SECRET = "rf7oeCzxfSeTYhnmYvJ4I27XCfCx0MVp"; // Replace with your client secret
 let REFRESH_TOKEN =
-  "pAUw2TvF4pkRZVCxmi6aka6wNhbupxvLf6GX24EQaeBeMzS5AmPhn1FrnAmhOf8z";
+  "maP95zQAzOvqjkx7HyOHJJFitCjHF0o78EZ4mnNoryBpN0L981z17mmwoS6TsYCy";
 const TOKEN_URL = "https://api.box.com/oauth2/token";
 const hostname = "127.0.0.1";
 const port = 3000;
@@ -22,6 +22,7 @@ const server = http.createServer(async (req, res) => {
     res.setHeader("Content-Type", "text/plain");
 
     // Format the data for application/x-www-form-urlencoded
+    console.log("Refresh_token", REFRESH_TOKEN);
     const data = querystring.stringify({
       grant_type: "refresh_token",
       refresh_token: REFRESH_TOKEN,
